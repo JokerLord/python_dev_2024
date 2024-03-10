5303edea8b67b3b0f46c32ae1db6f2f99d7c1102
murad <true.barsuk@yandex.ru>
2024-03-10 21:59:11 +0300

    Add make_bubble command

--- 04_MergetoolCommandline/cowsay_cmd.py
+++ 04_MergetoolCommandline/cowsay_cmd.py
@@ -12,6 +12,7 @@ class CowsayCMD(cmd.Cmd):
     def do_list_cows(self, args):
         """
         List of all cow file names in the given directory
+        
         list_cows [cow_path]
         """
         res = shlex.split(args)
@@ -20,6 +21,52 @@ class CowsayCMD(cmd.Cmd):
         else:
             print(*cowsay.list_cows(res[0]))
 
+    def do_make_bubble(self, args):
+        """
+        Wraps text is wrap_text is true, then pads text and sets inside a bubble.
+        This is the text that appears above the cows
+        
+        make_bubble [-b brackets] [-W width] [-w wrap_text] text
+
+        -b: Brackets type ("cowsay" or "cowthink", default: "cowsay")
+        -W: Bubble width (default: 40)
+        -w: Should text be wrapped ("True" or "False", default: "True")
+        """
+
+        params = {
+            "-b": cowsay.THOUGHT_OPTIONS["cowsay"],
+            "-W": 40,
+            "-w": True
+        }
+
+        res = shlex.split(args)
+        if len(res) in [1, 3, 5, 7]:
+            for i in range(0, len(res) - 1, 2):
+                if (res[i] == "-b") and (res[i + 1] in ["cowsay", "cowthink"]):
+                    params[res[i]] = cowsay.THOUGHT_OPTIONS[res[i + 1]]
+                elif res[i] == "-W":
+                    params[res[i]] = int(res[i + 1])
+                elif res[i] == "-w":
+                    if res[i + 1] == "True":
+                        params[res[i]] = True
+                    elif res[i + 1] == "False":
+                        params[res[i]] = False
+                    else:
+                        print("Invalid command syntax")
+                        return
+                else:
+                    print("Invalid command syntax")
+                    return
+        else:
+            print("Invalid command syntax")
+            return
+
+        print(cowsay.make_bubble(
+            res[-1],
+            brackets=params["-b"],
+            width=params["-W"],
+            wrap_text=params["-w"]
+        ))
 
 if __name__ == "__main__":
     CowsayCMD().cmdloop()
